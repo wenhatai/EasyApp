19888065868c51db6daf71acb6812b8e1bb31f48
parrzhang <wenhatai@gmail.com>
2014-07-23 21:48:28 +0800

    modify pandan

--- pandan/v1/index.php
+++ pandan/v1/index.php
@@ -7,13 +7,16 @@ require '.././libs/Slim/Slim.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/note', function() {
+$app->get('/note', function() use ($app){
+	$limit = $app->request->get('limit');
+	$page = $app->request->get('page');
 	$data = array();
 	for($i = 0;$i<10;$i++){
 		$note['id'] = $i;
 		$note['title'] = "title";
 		$note['content'] = "content";
 		$note['create_timestamp'] = time()-rand(0,100);
+		$note['resource_id'] = $i;
 		$resource['url'] = "http://rack.1.mshcdn.com/media/ZgkyMDE0LzA2LzI1LzU5L0FuZHJvaWQxLjE3YjNiLmpwZwpwCXRodW1iCTEyMDB4NjI3IwplCWpwZw/293f6197/1cb/Android1.jpg";
 		$resource['description'] = "description";
 		$resource['id'] = $i;
@@ -21,6 +24,10 @@ $app->get('/note', function() {
 		array_push($data, $note);
 	}
 	echo json_encode($data);
+});
+
+$app->get('/test',function() use ($app){
+	echo $app->request->get('a');
 });
 
 $app->run();
